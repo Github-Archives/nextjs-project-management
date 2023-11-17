9c4d48e845c4c66d3902da328fda1b53598f33a4
Github-Archives <drewreynerwilliams@gmail.com>
2023-11-17 03:50:47 -0500

    install tailwind and add configure files

--- pages/Test.js
+++ pages/Test.js
@@ -2,7 +2,11 @@
 import React from 'react';
 
 const Test = () => {
-  return <div>This is the Test page.</div>;
+  return (
+    <div className="z-1 mr-1 h-44 w-8">
+      This is the Test page.
+    </div>
+  );
 };
 
 export default Test;
