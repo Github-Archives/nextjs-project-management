9c56a1a61de146c1940095084f75ca53d60cdf1a
Github-Archives <drewreynerwilliams@gmail.com>
2023-11-17 00:37:49 -0500

    init commit

---
+++ pages/Test2.js
@@ -0,0 +1,8 @@
+// pages/Test2.js
+import React from 'react';
+
+const Test = () => {
+  return <div>This is the second Test page.</div>;
+};
+
+export default Test;
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,8 @@ const Home = () => {
     <div>
       <p>Hello, Next.js!</p>
       <Link href="/Test">Go to Test page</Link>
+      <p>Next link</p>
+      <Link href="/Test2">Go to Test2 page</Link>
     </div>
   );
 };
