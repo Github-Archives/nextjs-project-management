680ee61b68b2a0c9d275e81166ba0da7fdc37eeb
Drew Williams <15261678+Github-Archives@users.noreply.github.com>
2023-12-31 05:00:10 -0500

    render first row of db to screen (#5)

--- pages/PageA.js
+++ pages/PageA.js
@@ -2,28 +2,26 @@ import React, { useEffect, useState } from "react";
 import getData from "../utils/database";
 
 const PageA = () => {
-  const [databaseVersion, setDatabaseVersion] = useState("");
+  const [rowResponse, setRowResponse] = useState({});
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await getData();
-        const { version } = response[0]; // Extract the 'version' property from response
-        console.log(`Database Version:\n`, version);
-        setDatabaseVersion(version);
+        const getRowResponse = await getData();
+        setRowResponse(getRowResponse);
       } catch (error) {
-        // Handle errors here
         console.error("Error fetching data:", error);
       }
     }
-
     fetchData();
   }, []);
 
   return (
     <div>
-      <h1>Database Version:</h1>
-      <p>{databaseVersion}</p>
+      <h1>First Row of Database:</h1>
+      <p>{rowResponse.id}</p>
+      <p>{rowResponse.name}</p>
+      <p>{rowResponse.value}</p>
     </div>
   );
 };
