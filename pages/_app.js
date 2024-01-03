6f9861b8254e82f234f732e489743a790b872dab
Github-Archives <drewreynerwilliams@gmail.com>
2024-01-03 16:28:18 -0500

    add next-auth and initial file changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import "../styles/tailwind.css";
+// Original code:
+// import "../styles/tailwind.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
+// }
+
+// export default MyApp;
+
+import { SessionProvider } from 'next-auth/react';
+import '../styles/tailwind.css';
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
