ecd408ae00fa4b26ca7d139112f994a52ffc1732
Github-Archives <drewreynerwilliams@gmail.com>
2024-01-03 17:00:38 -0500

    add comment

--- components/login-btn.js
+++ components/login-btn.js
@@ -1,7 +1,7 @@
 import { useSession, signIn, signOut } from 'next-auth/react';
 
 export default function Component() {
-  const { data: session } = useSession();
+  const { data: session } = useSession(); // useSession Hook is the easiest way to check if someone is signed in. Must have <SessionProvider> added to pages/_app.js
   if (session) {
     return (
       <>
