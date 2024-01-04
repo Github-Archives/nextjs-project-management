ee56be434d8abcc5f086f4f6bb576b2a6cf77a80
Github-Archives <drewreynerwilliams@gmail.com>
2024-01-04 14:08:09 -0500

    attempted adding next-auth with NextJS /pages from a /app doc

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,21 @@
 // Original code:
-// import "../styles/tailwind.css";
-
-// function MyApp({ Component, pageProps }) {
-//   return <Component {...pageProps} />;
-// }
-
-// export default MyApp;
-
-import { SessionProvider } from 'next-auth/react';
 import '../styles/tailwind.css';
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
+
+// import { SessionProvider } from 'next-auth/react';
+// import '../styles/tailwind.css';
+
+// function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+//   return (
+//     <SessionProvider session={session}>
+//       <Component {...pageProps} />
+//     </SessionProvider>
+//   );
+// }
+
+// export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,20 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
+import Nav from './(components)/Nav';
 
 const Home = () => {
   return (
-    <div className="flex border-2 border-black">
-      <div>Welcome!</div>
-      <Link href="/PageA" className="mx-6 text-blue-500 underline">
-        Page A
-      </Link>
-      <Link href="/PageB" className="mx-6 text-blue-500 underline">
-        Page B
-      </Link>
+    // <div className="flex border-2 border-black">
+    //   <div>Welcome!</div>
+    //   <Link href="/PageA" className="mx-6 text-blue-500 underline">
+    //     Page A
+    //   </Link>
+    //   <Link href="/PageB" className="mx-6 text-blue-500 underline">
+    //     Page B
+    //   </Link>
+    // </div>
+    <div className="bg-gray-100">
+      <Nav />
     </div>
   );
 };
